2da14070d58aff47e0f492e52271174ca70e18e7
Alexandra Valerievna <a.valerievna12@gmail.com>
2018-11-22 20:56:40 +0300

    Initial commit

---
+++ src/test/java/framework/BaseTest.java
@@ -0,0 +1,11 @@
+package framework;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+    @BeforeClass
+    public void before(){}
+    @AfterClass
+    public void after() {}
+}
