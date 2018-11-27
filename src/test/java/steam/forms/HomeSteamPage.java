3a258f2dd8aa2e54a5a7785a3b579a304c2d3978
AValerievna <a.valerievna12@gmail.com>
2018-11-27 11:57:56 +0300

    getting cheepest game

--- src/test/java/steam/forms/HomeSteamPage.java
+++ src/test/java/steam/forms/HomeSteamPage.java
@@ -19,4 +19,19 @@ public class HomeSteamPage extends BasePage {
     public void goToActionSteamPage() {
         stMenu.navigateMenu(Tabs.GENRE.toString(),conf.getProperty("section.name"));
     }
+
+
+/*    LOAD BUNDLE EXAMPLE
+    ResourceBundle english = ResourceBundle.getBundle("localization", Locale.ENGLISH);
+    ResourceBundle russian = ResourceBundle.getBundle("localization", Locale.forLanguageTag("ru"));
+
+    RESOURCES FILES
+
+    localization_en.properties
+    localization_ru.properties
+
+    THE SAME AS CONFIGURATION CLASS WITH METHOD
+    public String getProperty(String propName, Locale locale) {
+        return  ResourceBundle.getBundle("localization", locale).getString(propName);
+    }*/
 }
